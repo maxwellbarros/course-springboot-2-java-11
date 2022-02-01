3562813b08add2bad27ba2e5867bdeb0c87a7608
Maxwell Barros <maxwellbarros@yahoo.com.br>
2022-02-01 20:04:18 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/well/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.well.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.well.course.entities.Order;
+import com.well.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long Id) {
+		Optional<Order> obj = repository.findById(Id);
+		return obj.get();
+	}
+}

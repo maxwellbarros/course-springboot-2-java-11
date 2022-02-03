73932d5764ed6284840897027349cb8c18ed53c7
Maxwell Barros <maxwellbarros@yahoo.com.br>
2022-02-03 17:37:36 -0300

    Category

---
+++ src/main/java/com/well/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.well.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.well.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ src/main/java/com/well/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.well.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.well.course.entities.Category;
+import com.well.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long Id) {
+		Optional<Category> obj = repository.findById(Id);
+		return obj.get();
+	}
+}

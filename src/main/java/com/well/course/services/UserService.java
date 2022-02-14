57c358df824026707ece7508a97ef4f930b67071
maxwellbarros <maxwellbarros@yahoo.com.br>
2022-02-14 16:08:09 -0300

    Exception handling - delete

--- src/main/java/com/well/course/services/UserService.java
+++ src/main/java/com/well/course/services/UserService.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 
 import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.well.course.entities.User;
 import com.well.course.repositories.UserRepository;
+import com.well.course.services.exceptions.DatabaseException;
 import com.well.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
